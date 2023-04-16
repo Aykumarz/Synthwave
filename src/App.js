ce3ca077c0f749b2845a640dcb9ba4e6b7003883
Ayush <aykumarze@gmail.com>
2023-04-16 20:59:26 +0530

    path updated

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => (
     <Box sx={{ backgroundColor: '#000' }}>
       <Navbar />
       <Routes>
-        <Route exact path='/Synthwave' element={<Feed />} />
+        <Route path='/' element={<Feed />} />
         <Route path='/video/:id' element={<VideoDetail />} />
         <Route path='/channel/:id' element={<ChannelDetail />} />
         <Route path='/search/:searchTerm' element={<SearchFeed />} />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { SearchBar } from "./";
 
 const Navbar = () => (
   <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
-    <Link to="/Synthwave" style={{ display: "flex", alignItems: "center", marginLeft: "35px"}}>
+    <Link to="/" style={{ display: "flex", alignItems: "center", marginLeft: "35px"}}>
       <img src={logo} alt="logo" height={45} />
     </Link>
     <SearchBar/>
